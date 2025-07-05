54b6347ca5ceaf1fcf9cbf85a9da712124f450ac
Riyad Hasan <riyadofficial.ide@gmail.com>
2025-07-05 06:12:40 +0600

    tooltip package added

---
+++ zero-btn.d.ts
@@ -0,0 +1,8 @@
+export function zeroTooltip(
+  element: string | Element | NodeListOf<Element> | HTMLCollection,
+  config?: {
+    tooltip?: string;
+    onTop?: boolean;
+    offset?: number;
+  }
+): void;
